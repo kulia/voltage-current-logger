2d17dcf4727a4658b48ca59ee9375c0c76d23d8c
Geir Kulia <geir@kulia.no>
2015-10-07 16:07:08 +0600

    move ino-file to src/

--- src/voltage-current-logger.ino
+++ src/voltage-current-logger.ino
@@ -1,8 +1,11 @@
 // This is a short arduino program used to log the current and voltage on a microgrid
 
-int voltage_pin = 0;
-int currnet_pin = 1;
+const int voltage_pin = 0;
+const int currnet_pin = 1;
 long t = 0;
+int v = 0;
+int i = 0;
+
 
 void setup() {
   // Start communication with the logger
@@ -12,10 +15,12 @@ void setup() {
 
 void loop() {
   t = micros();
+  v = analogRead(voltage_pin);
+  i = analogRead(currnet_pin);
   Serial.print(t); // Get timestamp on the measurement
   Serial.print(' ');
-  Serial.print(analogRead(voltage_pin)); // Send voltage measurement
-   Serial.print(' ');
+  Serial.print(v); // Send voltage measurement
+  Serial.print(' ');
   // The minus symbol is used to distinguish the volt measurements from the current measurements
-  Serial.println(analogRead(1)); // Send current measurement
+  Serial.println(i); // Send current measurement
 }
